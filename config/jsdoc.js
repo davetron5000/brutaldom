e511e84f68779f28eb56f27a552b878bb7fd40ee
Dave Copeland <davec@naildrivin5.com>
2023-09-04 15:50:32 -0400

    update doc theme

--- config/jsdoc.js
+++ config/jsdoc.js
@@ -7,11 +7,21 @@ module.exports = {
   source: {
     include: "src/js",
   },
+  markdown: {
+    hardwrap: false, // This is important, otherwise some features might not work.
+    idInHeadings: true // This is important, otherwise some features might not work.
+  },
   opts: {
     destination: "docs",
     encoding: "utf-8",
     recurse: true,
     readme: "README.md",
-  }
-
+    template: "node_modules/better-docs",
+  },
+  templates: {
+    "cleverLinks": true,
+    "search": true,
+    "better-docs": {
+    },
+  },
 };
